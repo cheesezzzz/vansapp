4186ead084434297512348d3ba5c5ab090933209
Huy Nguyen <113236181+cheesezzzz@users.noreply.github.com>
2023-05-27 22:50:03 +1000

    latest

--- src/pages/Host/HostVansDetail.jsx
+++ src/pages/Host/HostVansDetail.jsx
@@ -1,4 +1,13 @@
+import { useEffect, useState } from "react"
+
 export default function HostVansDetail() {
+    // TODO: Render the detail vans here dynamically
+    const [vanDetail, setVanDetail] = useState()
+
+    useEffect(() => {
+
+    })
+
     return (
         <h1>
             This is the Host Vans Detail page
